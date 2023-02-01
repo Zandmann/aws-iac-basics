3394a4b5e5871361329f5375bb08bac01f3031d3
Zandmann <piasecki.rd@gmail.com>
2023-02-01 18:03:52 +0000

    Updated main with correct tag information. Updated _backend with AWS bucket and dynamodb table name

--- lab_files/terraform_lab/_backend.tf
+++ lab_files/terraform_lab/_backend.tf
@@ -1,9 +1,9 @@
 terraform {
-    backend "s3" {
-        key = "finance/front-end-systems/terraform.tfstate"
-        region = "<<REGION>>"
-        bucket = "<<BUCKET NAME>>"
-        dynamodb_table = "<<DYNAMODB TABLE>>"
-        encrypt = true
-    }
+  backend "s3" {
+    key            = "finance/front-end-systems/terraform.tfstate"
+    region         = "us-east-1"
+    bucket         = "terraform-state-files-123456"
+    dynamodb_table = "terraform-locker"
+    encrypt        = true
+  }
 }
